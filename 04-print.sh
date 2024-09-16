4ab758b47a0fd3b6381a5da31473096d3378e4aa
Bharath kumar kuppala <bharath@simplifyvms.com>
2024-09-16 23:40:12 +0530

    changed to argument

--- 04-print.sh
+++ 04-print.sh
@@ -15,9 +15,9 @@ $ print color
   # [COLm  which color to enable, replace COL with 31-35
   #  [0m  to reset the color
 
-echo -e "\e31mTESTINGCOLOR Hello world in ReD color\e[0m"
-echo -e "\e32mTESTINGCOLOR Hello world in green color\e[0m"
-echo -e "\e33mTESTINGCOLOR Hello world in yellow color\e[0m"
-echo -e "\e34mTESTINGCOLOR Hello world in blue color\e[0m"
-echo -e "\e35mTESTINGCOLOR Hello world in Magneta color\e[0m"
-echo -e "\e36mTESTINGCOLOR Hello world in Cyan color\e[0m"
+echo -e "\e[31mTESTINGCOLOR Hello world in ReD color\e[0m"
+echo -e "\e[32mTESTINGCOLOR Hello world in green color\e[0m"
+echo -e "\e[33mTESTINGCOLOR Hello world in yellow color\e[0m"
+echo -e "\e[34mTESTINGCOLOR Hello world in blue color\e[0m"
+echo -e "\e[35mTESTINGCOLOR Hello world in Magneta color\e[0m"
+echo -e "\e[36mTESTINGCOLOR Hello world in Cyan color\e[0m"
